d3eee54944a283090c32311fb5e85158caf29678
Jaime Corona Jr <mitos2018_78@outlook.com>
2024-09-10 21:39:23 -0500

    Finished creating the recipe-list and the creating the functions in the script

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <script setup>
-import { ref} from 'vue';
+import { ref, onMounted, watch } from 'vue';
+
+const recipeName = ref('');
+const newIngredient = ref('');
+const newDirection = ref('');
+
+const recipe = ref([]);
+
+const addRecipe = () => {
+  if (newIngredient.value.trim() !== '' && newDirection.value.trim() !== '') {
+    recipe.value.push({
+      ingredient: newIngredient.value,
+      direction: newDirection.value,
+    });
+    // Clear the input fields after adding
+    newIngredient.value = '';
+    newDirection.value = '';
+  }
+};
+
+const removeRecipeItem = (index) => {
+  recipe.value.splice(index, 1);
+};
+
+onMounted(() => {
+  recipeName.value = localStorage.getItem('recipeName') || '';
+  recipe.value = JSON.parse(localStorage.getItem('recipe')) || [];
+});
 
 
+watch(recipeName, (newVal) => {
+  localStorage.setItem('recipeName', newVal);
+});
+
+
+watch(recipe, (newVal) => {
+  localStorage.setItem('recipe', JSON.stringify(newVal));
+}, { deep: true });
 
 </script>
 
@@ -17,20 +52,35 @@ import { ref} from 'vue';
       <h3>Create a Recipe</h3>
       <form @submit.prevent="addRecipe">
         <h4>What's the name of your recipe?</h4>
-        <input type="text" placeholder="e.g., Spaghetti Bolognese" v-model="recipeName" />
+        <input type="text" placeholder="e.g., Pizza" v-model="recipeName" />
 
         <h4>Add Ingredient</h4>
         <input type="text" placeholder="e.g., Tomatoes" v-model="newIngredient" />
 
         <h4>Add Directions</h4>
-        <input type="text" placeholder="e.g., Chop the tomatoes finely" v-model="newDirection" />
+        <input type="text" placeholder="e.g., Blend the tomatoes finely" v-model="newDirection" />
 
         <input type="submit" value="Add to Recipe" />
       </form>
     </section>
 
     <section class="recipe-list">
-    
+      <h3>Recipe: {{ recipeName }}</h3>
+      <div class="list">
+        <div v-for="(item, index) in recipe" :key="index" class="recipe-item">
+          <div class="recipe-content">
+            <strong>Ingredient:</strong>
+            <input type="text" v-model="item.ingredient" />
+
+            <strong>Direction:</strong>
+            <input type="text" v-model="item.direction" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeRecipeItem(index)">Delete</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
