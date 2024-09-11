a940f79b56a93462cdb27f2668f65c1a0168ce72
Jaime Corona Jr <mitos2018_78@outlook.com>
2024-09-10 21:30:44 -0500

    Finished creating the create recipe section

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,6 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref} from 'vue';
 
-// Recipe information
-const recipeName = ref('');
-const newIngredient = ref('');
-const newDirection = ref('');
-
-// Recipe list
-const recipe = ref([]);
-
-// Function to add a new recipe item (ingredient + direction)
-const addRecipe = () => {
-  if (newIngredient.value.trim() !== '' && newDirection.value.trim() !== '') {
-    recipe.value.push({
-      ingredient: newIngredient.value,
-      direction: newDirection.value,
-    });
-    // Clear the input fields after adding
-    newIngredient.value = '';
-    newDirection.value = '';
-  }
-};
-
-// Function to remove an item from the recipe list
-const removeRecipeItem = (index) => {
-  recipe.value.splice(index, 1);
-};
-
-// On mounted, load data from localStorage
-onMounted(() => {
-  recipeName.value = localStorage.getItem('recipeName') || '';
-  recipe.value = JSON.parse(localStorage.getItem('recipe')) || [];
-});
-
-// Watch for changes in `recipeName` and save to localStorage
-watch(recipeName, (newVal) => {
-  localStorage.setItem('recipeName', newVal);
-});
-
-// Watch for changes in the recipe list and save to localStorage
-watch(recipe, (newVal) => {
-  localStorage.setItem('recipe', JSON.stringify(newVal));
-}, { deep: true });
 
 
 </script>
@@ -71,22 +30,7 @@ watch(recipe, (newVal) => {
     </section>
 
     <section class="recipe-list">
-      <h3>Recipe: {{ recipeName }}</h3>
-      <div class="list">
-        <div v-for="(item, index) in recipe" :key="index" class="recipe-item">
-          <div class="recipe-content">
-            <strong>Ingredient:</strong>
-            <input type="text" v-model="item.ingredient" />
-
-            <strong>Direction:</strong>
-            <input type="text" v-model="item.direction" />
-          </div>
-
-          <div class="actions">
-            <button class="delete" @click="removeRecipeItem(index)">Delete</button>
-          </div>
-        </div>
-      </div>
+    
     </section>
   </main>
 </template>
